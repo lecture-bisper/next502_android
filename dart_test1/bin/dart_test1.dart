5ad0774c47bf6771324e65f0dc2d106e81868be2
DESKTOP-IVTH17U\it <bisper@outlook.kr>
2026-02-20 17:03:15 +0900

    2일차 - android : vscode 로 Dart 프로젝트 만들기

---
+++ dart_test1/bin/dart_test1.dart
@@ -0,0 +1,5 @@
+import 'package:args/args.dart';
+
+void main(List<String> arguments) {
+  print("다트 프로젝트 실행!!");
+}
---
+++ vscode_test1/test/vscode_test1_test.dart
@@ -0,0 +1,8 @@
+import 'package:vscode_test1/vscode_test1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
