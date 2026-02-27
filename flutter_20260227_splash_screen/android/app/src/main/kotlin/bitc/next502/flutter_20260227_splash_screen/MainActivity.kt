506b235a65b2f613e9b0b1d5d548549484753ac4
DESKTOP-IVTH17U\it <bisper@outlook.kr>
2026-02-27 16:58:03 +0900

    7일차 - Android : Flutter (플러터 앱 만들기, 이미지 등록)

---
+++ flutter_20260227_splash_screen/android/app/src/main/kotlin/bitc/next502/flutter_20260227_splash_screen/MainActivity.kt
@@ -0,0 +1,5 @@
+package bitc.next502.flutter_20260227_splash_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_20260227_splash_screen/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_20260227_splash_screen/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
+
